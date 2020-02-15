c0987261e7c05317dd94a61a033efcc8c5693656
David Durlan <david.a.durlan@gmail.com>
2020-02-15 18:40:54 +0100

    position computer man on page

--- public/script.js
+++ public/script.js
@@ -1,1 +1,13 @@
-import { gsap } from 'gsap';
+(function() {
+    var fishingLine = document.querySelector('.fishing-line');
+    var arm = document.querySelector('.arm');
+    console.log(arm);
+
+    // window.onmousemove = mouseMove;
+
+    function mouseMove(e) {
+        // fishingLine.style.top = e.clientY + 'px';
+        // fishingLine.style.width = e.clientX + 'px';
+        console.log(arm.offsetLeft);
+    }
+})();
