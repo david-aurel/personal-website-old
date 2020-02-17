386c3c73257cd76c6214cb97cf51d480c37d7f26
David Durlan <david.a.durlan@gmail.com>
2020-02-17 18:55:41 +0100

    progress on nav slider animation

--- public/script.js
+++ public/script.js
@@ -1,22 +1,31 @@
 (function() {
     var pages = document.querySelectorAll('.page');
     var nav = document.querySelector('nav');
-    var navElems = document.querySelectorAll('nav p');
-    navElems.forEach(function(elem) {
-        elem.addEventListener('click', function(e) {
-            pages.forEach(function(elem) {
-                elem.classList.remove('show');
-            });
-            navElems.forEach(function(elem) {
-                elem.classList.remove('active');
-            });
+    var navElems = document.querySelectorAll('.navLinks div');
+    var slider = document.querySelector('.slider');
 
-            for (var i = 0; i < navElems.length; i++) {
-                if (e.target == navElems[i]) {
-                    pages[i].classList.add('show');
-                }
-            }
-            e.target.classList.add('active');
+    nav.addEventListener('click', function(e) {
+        pages.forEach(function(elem) {
+            elem.classList.remove('show');
         });
+        for (var i = 0; i < navElems.length; i++) {
+            if (e.target == navElems[i]) {
+                pages[i].classList.add('show');
+            }
+        }
+
+        var sliderLeft;
+        switch (e.target) {
+            case navElems[0]:
+                sliderLeft = '0%';
+                break;
+            case navElems[1]:
+                sliderLeft = '39%';
+                break;
+            case navElems[2]:
+                sliderLeft = '76%';
+                break;
+        }
+        slider.style.marginLeft = sliderLeft;
     });
 })();
