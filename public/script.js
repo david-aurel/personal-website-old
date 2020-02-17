fd397c84272976358b347d972160f03daab74ecf
David Durlan <david.a.durlan@gmail.com>
2020-02-17 17:37:26 +0100

    nav links work

--- public/script.js
+++ public/script.js
@@ -2,16 +2,21 @@
     var pages = document.querySelectorAll('.page');
     var nav = document.querySelector('nav');
     var navElems = document.querySelectorAll('nav p');
-    nav.addEventListener('click', function(e) {
-        pages.forEach(function(elem) {
-            elem.classList.remove('show');
-        });
-        for (var i = 0; i < navElems.length; i++) {
-            if (e.target == navElems[i]) {
-                pages[i].classList.add('show');
+    navElems.forEach(function(elem) {
+        elem.addEventListener('click', function(e) {
+            pages.forEach(function(elem) {
+                elem.classList.remove('show');
+            });
+            navElems.forEach(function(elem) {
+                elem.classList.remove('active');
+            });
+
+            for (var i = 0; i < navElems.length; i++) {
+                if (e.target == navElems[i]) {
+                    pages[i].classList.add('show');
+                }
             }
-            navElems[i].classList.remove('active');
-        }
-        e.target.classList.toggle('active');
+            e.target.classList.add('active');
+        });
     });
 })();
