68fac2171eabf274f5a199102ebb23d305fe0275
David Durlan <david.a.durlan@gmail.com>
2020-02-18 09:52:05 +0100

    slider looking fresh!

--- public/script.js
+++ public/script.js
@@ -3,28 +3,36 @@
     var nav = document.querySelector('nav');
     var navElems = document.querySelectorAll('.navLink');
     var slider = document.querySelector('.slider');
+    var content = document.querySelector('#contentWrapper');
 
     nav.addEventListener('click', function(e) {
-        pages.forEach(function(elem) {});
-        for (var i = 0; i < navElems.length; i++) {
-            if (e.target == navElems[i]) {
-                document.querySelector('.show').classList.remove('show');
-                pages[i].classList.add('show');
-            }
-        }
+        // pages.forEach(function(elem) {});
+        // for (var i = 0; i < navElems.length; i++) {
+        //     if (e.target == navElems[i]) {
+        //         document.querySelector('.show').classList.remove('show');
+        //         pages[i].classList.add('show');
+        //     }
+        // }
+        // var position = e.target.getAttribute('data-pos');
+        // var translateValue = translateX(position);
 
-        var sliderLeft;
+        var sliderPos;
+        var contentPos;
         switch (e.target) {
             case navElems[0]:
-                sliderLeft = '0%';
+                sliderPos = '0%';
+                contentPos = '0%';
                 break;
             case navElems[1]:
-                sliderLeft = '39%';
+                sliderPos = '39%';
+                contentPos = '-33.34%';
                 break;
             case navElems[2]:
-                sliderLeft = '76%';
+                sliderPos = '76%';
+                contentPos = '-66.67%';
                 break;
         }
-        slider.style.marginLeft = sliderLeft;
+        slider.style.marginLeft = sliderPos;
+        content.style.transform = `translateX(${contentPos})`;
     });
 })();
