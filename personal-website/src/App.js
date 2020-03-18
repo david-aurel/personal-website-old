1304bd15bc801520cf44a526c5abb1bdc0dc5a01
David Durlan <david.a.durlan@gmail.com>
2020-03-18 19:13:47 +0100

    get a little react router going

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
+import About from './components/about';
+import Portfolio from './components/portfolio';
+import Contact from './components/contact';
 
 function App() {
-    return <div className='App'></div>;
+    return (
+        <Router>
+            <div className='App'>
+                <nav>
+                    <p>
+                        <Link to='/about'>About</Link>
+                    </p>
+                    <p>
+                        <Link to='/portfolio'>Portfolio</Link>
+                    </p>
+                    <p>
+                        <Link to='/contact'>Contact</Link>
+                    </p>
+                </nav>
+                <Switch>
+                    <Route path='/about'>
+                        <About />
+                    </Route>
+                    <Route path='/portfolio'>
+                        <Portfolio />
+                    </Route>
+                    <Route path='/contact'>
+                        <Contact />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ personal-website/src/components/contact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contact = () => {
+    return <p>CONTACT</p>;
+};
+
+export default Contact;
